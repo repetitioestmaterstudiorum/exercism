72a9365fa6412dc5ba5ce73b281b547319a70ef0
repetitioestmaterstudiorum <thotan@ik.me>
2021-05-24 17:02:52 +0200

    comments

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -3,7 +3,9 @@ def hello():
 
 error_message = f"hello() doesn't print 'Hello, World!', it prints {hello()}"
 
+# break
 if not hello() == 'Hello, World!':
     raise Exception(error_message)
 
+# just log
 assert (hello() == 'Hello, World!'), error_message
