b6a2dd1b695ce2b459fb3eb1e3c4d4384a8e065c
repetitioestmaterstudiorum <thotan@ik.me>
2021-06-23 21:18:49 +0200

    ex 5 version 2

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,22 +1,19 @@
 class Matrix:
     def __init__(self, matrix_string):
-        def split_to_int_list(str):
-            return list(map(int, str.split(' ')))
-        self.matrix_rows = list(map(split_to_int_list, matrix_string.split('\n')))
+        self.matrix_rows = [x.split() for x in matrix_string.splitlines()]
 
     def row(self, index):
-        return self.matrix_rows[index - 1]
+        return [int(x) for x in self.matrix_rows[index - 1]]
 
     def column(self, index):
-        return [x[index - 1] for x in self.matrix_rows if len(x) >= index]
-
+        return [int(x[index - 1]) for x in self.matrix_rows if len(x) >= index]
 
 # dev assertions
-# matrix = "9 88 7\n5 3 2\n6 6"
-# m = Matrix(matrix)
-# assert(m.row(1) == [9, 88, 7])
-# assert(m.row(2) == [5, 3, 2])
-# assert(m.row(3) == [6, 6])
-# assert(m.column(1) == [9, 5, 6]) 
-# assert(m.column(2) == [88, 3, 6])
-# assert(m.column(3) == [7, 2])
+matrix = "9 88 7\n5 3 2\n6 6"
+m = Matrix(matrix)
+assert(m.row(1) == [9, 88, 7])
+assert(m.row(2) == [5, 3, 2])
+assert(m.row(3) == [6, 6])
+assert(m.column(1) == [9, 5, 6]) 
+assert(m.column(2) == [88, 3, 6])
+assert(m.column(3) == [7, 2])
