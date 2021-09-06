8ddf2fdfa91bc11f9306b4decaad18704aad731e
repetitioestmaterstudiorum <thotan@ik.me>
2021-09-06 21:23:51 +0200

    next iteration on the protein translatio problem

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,12 +1,15 @@
 import re
 from itertools import takewhile 
 
+# ---------
+
 def proteins(strand):
     codons_in_strand = re.findall('.{3}', strand)
-    codons_before_stop = takewhile(lambda c: c not in ["UAA", "UAG", "UGA"], codons_in_strand)
-    return [protein for codon in codons_before_stop for protein, codon_list in codon_to_protein.items() if codon in codon_list]
+    codons_before_stop = takewhile(lambda c: c not in stop_codons, codons_in_strand)
+    return [codon_to_protein[c] for c in codons_before_stop]
 
-codon_to_protein = {
+# --- human editable information
+protein_to_codon = {
         "Methionine": ["AUG"],
         "Phenylalanine": ["UUU", "UUC"],
         "Leucine": ["UUA", "UUG"],
@@ -15,5 +18,10 @@ codon_to_protein = {
         "Cysteine": ["UGU", "UGC"],
         "Tryptophan": ["UGG"],
     }
+stop_codons = ["UAA", "UAG", "UGA"]
 
-print(proteins("AUGUUUUCUUAAAUG"))
+# --- dict data for proteins() function
+codon_to_protein = {c: p for p, c_l in protein_to_codon.items() for c in c_l}
+
+# --- quick test
+# print(proteins("AUGUUUUCUUAAAUG"))
