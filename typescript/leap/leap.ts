7c171f9175a0fcc8052e95cb7991377fdf800891
repetitioestmaterstudiorum <thotan@ik.me>
2021-11-26 17:21:56 +0100

    leap exercise

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,4 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+export function isLeap(year: number = new Date().getFullYear()): boolean {
+	const isYearDivisibleBy = (number: number) => year % number === 0
+	return isYearDivisibleBy(4) && isYearDivisibleBy(100) || isYearDivisibleBy(400)
 }
