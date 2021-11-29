fe08de6d144e78d90a75f3f39450c1f59e2ee6c1
repetitioestmaterstudiorum <thotan@ik.me>
2021-11-29 22:24:02 +0100

    better naming

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -2,7 +2,7 @@ export function toRna(dna: string): string {
 	type DnaNucleotides = 'G' | 'C' | 'T' | 'A'
 	type RnaNucleotides = 'C' | 'G' | 'A' | 'U'
 
-	const DnaRnaTranscription: Record<DnaNucleotides, RnaNucleotides> = {
+	const dnaToRna: Record<DnaNucleotides, RnaNucleotides> = {
 		'G': 'C',
 		'C': 'G',
 		'T': 'A',
@@ -12,8 +12,8 @@ export function toRna(dna: string): string {
 	return dna
 		.split('')
 		.map(n => {
-			if (!(n in DnaRnaTranscription)) throw new Error('Invalid input DNA.')
-			return DnaRnaTranscription[n as DnaNucleotides]
+			if (!(n in dnaToRna)) throw new Error('Invalid input DNA.')
+			return dnaToRna[n as DnaNucleotides]
 		})
 		.join('')
 }
