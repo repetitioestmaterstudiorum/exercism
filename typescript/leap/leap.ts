7d0630147bbe7c2946c95b8199e5f9cedd4c0878
repetitioestmaterstudiorum <thotan@ik.me>
2022-02-20 18:17:45 +0100

    correct leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,4 +1,4 @@
 export function isLeap(year: number = new Date().getFullYear()): boolean {
 	const isYearDivisibleBy = (number: number) => year % number === 0
-	return isYearDivisibleBy(4) && isYearDivisibleBy(100) || isYearDivisibleBy(400)
+	return (isYearDivisibleBy(4) && !isYearDivisibleBy(100)) || isYearDivisibleBy(400)
 }
